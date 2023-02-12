31b6d124c6063279a7a70f9a408045416a9938b0
unknown <dnpffldxjs12@gmail.com>
2023-02-12 19:07:15 +0900

    1157

---
+++ bj_pb/1157.py
@@ -0,0 +1,19 @@
+a = input()
+a = a.upper()
+b = set(a)
+b = list(b)
+count = [0] * len(b)
+for word in a:
+    count[b.index(word)] += 1
+
+
+l = count.index(max(count))
+r = count[::-1].index(max(count))
+
+print(count)
+print(b)
+
+if l != len(count) - 1 - r:
+    print('?')
+else:
+    print(b[l])
