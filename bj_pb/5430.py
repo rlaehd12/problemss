356ac8c5616f729d2140d245c19c4f05904161f1
rlaehd12 <dnpffldxjs12@gmail.com>
2023-02-17 17:51:19 +0900

    1235

---
+++ bj_pb/5430.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open("5430.txt")
+
+
+t = int(input())
+
+for tc in range(1,t+1):
+    order = input()
+    n = int(input())
+    lst = input()[1:-1].split(',')
+
+    if n !=0:
+        lst = list(map(int, lst))
+    else:
+        lst = []
+    a = 0
+    pos = [0, -1]
+    for word in order:
+        if word == 'R':
+            a = (a+1)%2
+        elif word == 'D':
+            if lst:
+                lst.pop(pos[a])
+            else:
+                print('error')    
+                break
+    else:
+        if a == 0:
+            print(lst)
+        else:
+            lst.reverse()
+            print(lst)
---
+++ bj_pb/9663.py
@@ -0,0 +1,91 @@
+def permu(i, n):
+    global cnt
+    if i == n:  # base case
+        cnt += 1
+    else:
+        for j in range(i, n):  # 현재 i-1인덱스까지 고정된 상태
+            s = 0
+            lst[i], lst[j] = lst[j], lst[i]  # 자리 바꿈
+            
+            if right_check[i + (n-1)-lst[i]] == 1 or left_check[i + lst[i]] == 1:
+                lst[i], lst[j] = lst[j], lst[i]  # 조건에 맞지 않으면 다시 원상복귀
+                continue
+            else:
+                right_check[i + (n - 1) - lst[i]] += 1
+                left_check[i + lst[i]] += 1
+
+            permu(i+1, n)
+            # 초기화
+            right_check[i + (n - 1) - lst[i]] -= 1
+            left_check[i + lst[i]] -= 1
+            lst[i], lst[j] = lst[j], lst[i]
+
+
+n = int(input())
+lst = [i for i in range(n)]
+right_check = [0] * ((2*n)-1)  # 대각선 방향 체크
+left_check = [0] * ((2*n)-1)
+cnt = 0
+permu(0, n)
+print(cnt)
+
+############## 똑같은데 더 깔끔한거
+# n = int(input())
+# cnt = 0
+# is_used1 = [False] * n
+# is_used2 = [False] * (2*n - 1)
+# is_used3 = [False] * (2*n - 1)
+# 
+# def func(k):
+#     global cnt
+#     if k == n:
+#         cnt += 1
+#         return
+#     for i in range(n):
+#         if is_used1[i] or is_used2[i - k + n - 1] or is_used3[i + k]:
+#             continue
+#         is_used1[i] = True
+#         is_used2[i - k + n - 1] = True
+#         is_used3[i + k] = True
+#         func(k+1)
+#         is_used1[i] = False
+#         is_used2[i - k + n - 1] = False
+#         is_used3[i + k] = False
+# 
+# func(0)
+# print(cnt)
+
+
+# def permu(i, n):
+#     global cnt
+#     global call
+#     call += 1
+#     if i == n:  # base case
+#         if not check(i):
+#             cnt += 1
+#     else:
+#         for j in range(i, n):
+#             s = 0
+#             lst[i], lst[j] = lst[j], lst[i]
+#             if check(i):
+#                 return
+#             permu(i+1, n)
+#             lst[i], lst[j] = lst[j], lst[i]
+#
+#
+# def check(i):
+#     global call2
+#     call2 += 1
+#     for j in range(i-1):
+#         if abs(lst[j] - lst[i-1]) == abs(j-(i-1)):
+#             return True
+#     return False
+
+
+# n = int(input())
+# lst = [i for i in range(n)]
+# cnt = 0
+# call = call2 = 0
+# permu(0, n)
+# print(cnt)
+# print(call, call2)
