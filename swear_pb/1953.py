dc782eb3340cbad469ebabb235670f383841f64c
rlaehd12 <dnpffldxjs12@gmail.com>
2023-02-24 17:38:30 +0900

    325

--- swear_pb/1953.py
+++ swear_pb/1953.py
@@ -3,7 +3,7 @@ sys.stdin= open("1953.txt")
 
 p = [[], [0,1,2,3], [0,1], [2,3], [0,3], [1,3], [1,2], [0,2]]  # 파이프
 opp = {0:1, 1:0, 2:3, 3:2}  # 연결되야할 방향
-di, dj = [-1,1,0,0], [0,0,-1,1]  # 상하좌우
+di,dj = [-1,1,0,0],[0,0,-1,1]  # 상하좌우
 
 def bfs(si, sj, l):
     q = []
@@ -14,22 +14,22 @@ def bfs(si, sj, l):
     cnt = 1
 
     while q:
-        ci, cj = q.pop()
+        ci, cj = q.pop(0)
         if v[ci][cj] == l:
-            for i in v:
-                print(i)
+            # for i in v:
+            #     print(i)
             return cnt
 
         for dr in p[arr[ci][cj]]:
             ni, nj = ci+di[dr], cj+dj[dr]
-            if 0<=ni<n and 0<=nj<n and v[ni][nj] == 0 and\
+            if 0<=ni<n and 0<=nj<m and v[ni][nj] == 0 and\
                 opp[dr] in p[arr[ni][nj]]:
                 q.append((ni,nj))
                 v[ni][nj] = v[ci][cj]+1
                 cnt += 1
     # 공간이 좁아 L시간 전에 모두 순회한 경우
-    for i in v:
-        print(i)
+    # for i in v:
+    #     print(i)
     return cnt
 
 
--- swear_pb/4408live.py
+++ swear_pb/4408live.py
@@ -3,26 +3,40 @@ sys.stdin = open("4408.txt")
 
 t = int(input())
 
-# for tc in range(1,t+1):
-#     n = int(input())
-
-#     lst = [tuple(map(int, input().split())) for _ in range(n)]
-#     print(lst)
-
-#     lst.sort(key=lambda x: x[1])
-#     print(lst)
-
-
 for tc in range(1,t+1):
     n = int(input())
-    cnts = [0] * 200
-    for _ in range(n):
-        s, e = map(int, input().split())
-        if s > e:
-            s, e = e, s
 
-        for i in range((s-1)//2, (e-1)//2+1):
-            cnts[i] += 1
-    ans = max(cnts)
+    lst = [list(map(int, input().split())) for _ in range(n)]
+    for i in range(n):
+        if lst[i][0] > lst[i][1]:
+            lst[i][0], lst[i][1] = lst[i][1], lst[i][0]
 
-    print(f'#{tc} {ans}')
+    lst.sort(key=lambda x: x[0])
+    lst.sort(key=lambda x: x[1])
+    # print(lst)
+
+    cur = lst[0]
+    cnt = 1
+
+    for i in range(1,n):
+        if cur[1] <= lst[i][0]:
+            cnt += 1
+            cur = lst[i]
+    
+    print(f'#{tc} {cnt}')
+
+
+
+# for tc in range(1,t+1):
+#     n = int(input())
+#     cnts = [0] * 200
+#     for _ in range(n):
+#         s, e = map(int, input().split())
+#         if s > e:
+#             s, e = e, s
+
+#         for i in range((s-1)//2, (e-1)//2+1):
+#             cnts[i] += 1
+#     ans = max(cnts)
+
+#     print(f'#{tc} {ans}')
